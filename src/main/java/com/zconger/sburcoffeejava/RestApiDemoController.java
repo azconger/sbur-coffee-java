9c70e7f2ac65831b8fe0d5786a5abe696a08fd35
Zachary Conger <zachary.conger@stackhawk.com>
2022-01-04 22:08:46 -0700

    Got a working controller GET mapping

--- src/main/java/com/zconger/sburcoffeejava/RestApiDemoController.java
+++ src/main/java/com/zconger/sburcoffeejava/RestApiDemoController.java
@@ -1,5 +1,6 @@
 package com.zconger.sburcoffeejava;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,4 +8,18 @@ import java.util.List;
 @RestController
 public class RestApiDemoController {
     private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Café Cereza"),
+                new Coffee("Café Blurdgerk"),
+                new Coffee("Café Flemtoblaz"),
+                new Coffee("Café Gleebovlarts")
+        ));
+    }
+
+    @GetMapping(value = "/coffees")
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
 }
