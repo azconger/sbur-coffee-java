4c0885b871a005aeb2ba67c75b7c89c9d5ac976f
Zachary Conger <zachary.conger@stackhawk.com>
2022-01-06 21:40:22 -0700

    added POST and PUT

--- src/main/java/com/zconger/sburcoffeejava/RestApiDemoController.java
+++ src/main/java/com/zconger/sburcoffeejava/RestApiDemoController.java
@@ -1,11 +1,14 @@
 package com.zconger.sburcoffeejava;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/")
 public class RestApiDemoController {
     private List<Coffee> coffees = new ArrayList<>();
 
@@ -21,5 +24,34 @@ public class RestApiDemoController {
     @GetMapping(value = "/coffees")
     Iterable<Coffee> getCoffees() {
         return coffees;
+    }
+
+    @GetMapping(value = "/coffees/{id}")
+    Optional<Object> getCoffeeById(@PathVariable String id) {
+        for (Coffee c: coffees) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @PostMapping(value = "/coffees")
+    Coffee postCoffee(@RequestBody Coffee coffee) {
+        coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping(value = "/coffees/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1;
+         for (Coffee c: coffees) {
+             if (c.getId().equals(id)) {
+                 coffeeIndex = coffees.indexOf(c);
+                 coffees.set(coffeeIndex, coffee);
+             }
+         }
+        return coffeeIndex == -1 ? postCoffee(coffee) : coffee;
     }
 }
