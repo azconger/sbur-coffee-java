6f0b8b446a1c50fb9c8c192f04c5855a05121040
Zachary Conger <zachary.conger@stackhawk.com>
2023-01-13 11:53:12 -0700

    added StackHawk config and OpenAPI spec

--- src/main/java/com/zconger/sburcoffeejava/Coffee.java
+++ src/main/java/com/zconger/sburcoffeejava/Coffee.java
@@ -3,16 +3,16 @@ package com.zconger.sburcoffeejava;
 import java.util.UUID;
 
 class Coffee {
-    private final String id;
-    private String name;
+    private final String id;    // the ID of the coffee (final, so it's a constant)
+    private String name;        // the name of the coffee
 
-    public Coffee(String id, String name) {
+    public Coffee(String id, String name) {     // constructor
         this.id = id;
         this.name = name;
     }
 
     public Coffee(String name) {
-        this(UUID.randomUUID().toString(), name);
+        this(UUID.randomUUID().toString(), name);   // constructor
     }
 
     public String getName() {
@@ -26,4 +26,6 @@ class Coffee {
     public String getId() {
         return id;
     }
+
+    // there is no setId because the ID for a coffee object is immutable
 }
